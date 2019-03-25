0e10ffb9205070d0c9c406e179fed6f89c18f71b
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-25 16:30:10 +0000

    Update sugarcog.py

--- sugarcog/sugarcog.py
+++ sugarcog/sugarcog.py
@@ -4,24 +4,24 @@ import time
 import random
 
 class sugarcog:
-    """Sugarcog!"""
+    """Sugarcane farm!!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def sugarcane(self,times):
+    async def sugarcane(self,times=5):
         """Sugar Cane!"""
         times=int(times)
         msg=self.bot.say("<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
         for i in range(times):
-            pause=random.randint(1,15)
+            pause=random.randint(4,15)
             time.sleep(pause)
             self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:sugarcane:559556252972220478> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
-            pause=random.randint(1,15)
+            pause=random.randint(4,15)
             time.sleep(pause)
             self.bot.edit_message(msg,new_content="<:sugarcane:559556252972220478> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:sugarcane:559556252972220478> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
-            pause=random.randint(1,15)
+            pause=random.randint(4,15)
             time.sleep(pause)
             self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
 
