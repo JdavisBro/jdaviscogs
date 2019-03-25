d6f3bf39626e54e957588fbfdac0d4c9478c6778
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-25 02:40:09 +0000

    Update sugarcogs.py

--- sugarcogs/sugarcogs.py
+++ sugarcogs/sugarcogs.py
@@ -14,7 +14,7 @@ class sugarcogs:
         """Sugar Cane!"""
         times=int(times)
         msg=self.bot.say("<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
-        for _ in range(times):
+        for i in range(times):
             pause=random.randint(1,15)
             time.sleep(pause)
             self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:sugarcane:559556252972220478> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
