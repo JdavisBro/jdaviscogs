5b67c268ac9254f7b44102eaca7fdbe11f6e2e1b
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-25 02:41:32 +0000

    Update sugarcogs.py

--- sugarcogs/sugarcogs.py
+++ sugarcogs/sugarcogs.py
@@ -23,7 +23,7 @@ class sugarcogs:
             self.bot.edit_message(msg,new_content="<:sugarcane:559556252972220478> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:sugarcane:559556252972220478> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
             pause=random.randint(1,15)
             time.sleep(pause)
-            self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>\")
+            self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>")
 
 def setup(bot):
     bot.add_cog(sugarcogs(bot))
