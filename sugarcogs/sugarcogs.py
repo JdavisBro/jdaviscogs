8bc4334b33e849306c8c968387d47e80f361dba9
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-25 02:27:42 +0000

    Update sugarcogs.py

--- sugarcogs/sugarcogs.py
+++ sugarcogs/sugarcogs.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import time
 import random
 
-class sugarcog:
+class sugarcogs:
     """Sugarcog!"""
 
     def __init__(self, bot):
@@ -26,4 +26,4 @@ async def sugarcane(self,times):
         self.bot.edit_message(msg,new_content="<:space:559559231817121793> <:observer:559556253807017985> <:redstone:559556253081272320> \n<:space:559559231817121793> <:piston:559556586650206223> <:stone:559556253169483796> \n<:sugarcane:559556252972220478>\")
 
 def setup(bot):
-    bot.add_cog(sugarcog(bot))
+    bot.add_cog(sugarcogs(bot))
