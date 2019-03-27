7b3371090508ce5bc14bc44307bf776165cd9ea1
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-27 21:41:32 +0000

    Create saymeanthings.py

---
+++ saymeanthings/saymeanthings.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import time
+import random
+
+class saymeanthings:
+    """Mean!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def meanthing(self,ctx,*,mean_thing):
+        """Mean!"""
+        time.sleep(2)
+        await self.bot.send_typing(ctx.message.channel)
+        sleepfor=random.randint(2,6)
+        time.sleep(sleepfor)
+        await self.bot.say("WOW IM LITERALLY CRYING AND SHAKING I CAN'T BELIEVE {} SAID `{}` TO ME 😢 😰".format(ctx.message.author.display_name,mean_thing))
+
+def setup(bot):
+    bot.add_cog(saymeanthings(bot))
