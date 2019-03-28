1e52deed9163fefcff97d7934ce5f1a64bcc7a36
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 23:53:38 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -28,7 +28,7 @@ class getmessages:
         percent=0
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
-        asyncio.run(channel())
+        await asyncio.run(channel())
         if server==0:
             await self.bot.say("You have said {} {} times out of the server's {} times! That makes an error".format(contains,person,server))
         else:
