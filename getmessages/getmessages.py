6c7c854bd80b4212e0c7a1d45ced44708cbc382d
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 23:46:23 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class getmessages:
     """getmessages!"""
@@ -15,13 +16,15 @@ class getmessages:
         percent=0
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
-            for channel in ctx.message.server.channels:
-                yield channel
-                async for message in client.logs_from(channel):
-                    if contains in message.content:
-                        server+=1
-                        if message.author==ctx.message.author:
-                            person+=1
+            async def server():
+                async for channel in ctx.message.server.channels:
+                    yield channel
+                    async for message in client.logs_from(channel):
+                        if contains in message.content:
+                            server+=1
+                            if message.author==ctx.message.author:
+                                person+=1
+        await asyncio.run(server())
         if server==0:
             await self.bot.say("You have said {} {} times out of the server's {} times! That makes an error".format(contains,person,server))
         else:
