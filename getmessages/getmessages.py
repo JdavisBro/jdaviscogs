abc7329992e44b458f9d646565362ff1947dd769
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 22:22:23 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -12,8 +12,8 @@ class getmessages:
     @commands.command(pass_context=True)
     async def getmessages(self,ctx,*,contains):
         """Get messages!"""
-        int(person=0)
-        int(server=0)
+        person=0
+        server=0
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
         for channel in ctx.message.server.channels:
@@ -24,9 +24,9 @@ class getmessages:
                     server+1
                     if message.author==ctx.message.author:
                         person+1
-        int(percent=person/server*100)
+        percent=person/server*100
         await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
-                    
+
 
 def setup(bot):
     bot.add_cog(getmessages(bot))
