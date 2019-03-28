4b23d3ca77c7313ed62100284220c6a9d4c5daea
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 23:39:59 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -16,12 +16,12 @@ class getmessages:
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
             for channel in ctx.message.server.channels:
-                async def get_logs_from(channel):
-                    for m in client.logs_from(channel):
-                        if contains in message.content:
-                            server+=1
-                            if message.author==ctx.message.author:
-                                person+=1
+                yield channel
+                async for message in client.logs_from(channel):
+                    if contains in message.content:
+                        server+=1
+                        if message.author==ctx.message.author:
+                            person+=1
         if server==0:
             await self.bot.say("You have said {} {} times out of the server's {} times! That makes an error".format(contains,person,server))
         else:
