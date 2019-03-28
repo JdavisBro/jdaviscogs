b6f08a897a2c04282e2f44810696274ff1c68481
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 22:56:57 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -12,17 +12,18 @@ class getmessages:
         """Get messages!"""
         person=int(0)
         server=int(0)
+        contains=str(contains)
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
         for channel in ctx.message.server.channels:
             channel=yield channel
-            logs=await self.bot.logs_from(channel,limit=1000000)
+            logs=await self.bot.logs_from(channel,limit=100000)
             for message in logs:
                 if contains in message.content:
                     server=server+1
                     if message.author==ctx.message.author:
                         person=person+1
-        str(percent=person/server*100)
+        percent=person/server*100
         await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
 
 
