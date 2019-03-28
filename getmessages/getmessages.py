4fc4bffc3463c749c3e6a32701ec64b52791d089
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 23:51:22 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -16,21 +16,24 @@ class getmessages:
         percent=0
         await self.bot.say("Checking server for {} by you!".format(contains))
         await self.bot.send_typing(ctx.message.channel)
-            async def server():
-                async for channel in ctx.message.server.channels:
-                    yield channel
-                    async for message in client.logs_from(channel):
-                        if contains in message.content:
-                            server+=1
-                            if message.author==ctx.message.author:
-                                person+=1
-        await asyncio.run(server())
+        asyncio.run(channel())
         if server==0:
             await self.bot.say("You have said {} {} times out of the server's {} times! That makes an error".format(contains,person,server))
         else:
             percent=person/server*100
             await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
 
+async def message():
+    async for message in client.logs_from(channel):
+        if contains in message.content:
+            server+=1
+            if message.author==ctx.message.author:
+                person+=1
+
+async def channel():
+    async for channel in ctx.message.server.channels:
+        yield channel
+        asyncio.run(message())
 
 def setup(bot):
     bot.add_cog(getmessages(bot))
