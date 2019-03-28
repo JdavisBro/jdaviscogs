bffb7a26aaeef7d0a7ceff5f0b2bfe62d3ddfdf9
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 22:41:49 +0000

    pls work

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -19,10 +19,10 @@ class getmessages:
             logs=await self.bot.logs_from(channel,limit=1000000)
             for message in logs:
                 if contains in message.content:
-                    server+1
+                    server=server+1
                     if message.author==ctx.message.author:
-                        person+1
-        percent=person/server*100
+                        person=person+1
+        str(percent=person/server*100)
         await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
 
 
