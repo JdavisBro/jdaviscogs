616dd8f6d20a5cd34cc79f5eac0111445256a545
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 23:24:59 +0000

    Update getmessages.py

--- getmessages/getmessages.py
+++ getmessages/getmessages.py
@@ -22,8 +22,11 @@ class getmessages:
                         server+=1
                         if message.author==ctx.message.author:
                             person+=1
-            percent=person/server*100
-            await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
+            if server==0:
+                await self.bot.say("You have said {} {} times out of the server's {} times! That makes an error".format(contains,person,server))
+            else:
+                percent=person/server*100
+                await self.bot.say("You have said {} {} times out of the server's {} times! That makes {}%".format(contains,person,server,percent))
 
 
 def setup(bot):
