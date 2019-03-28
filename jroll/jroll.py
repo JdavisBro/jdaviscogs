057c51509313b24a185848482278a7270207568e
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-03-28 17:34:51 +0000

    Update jroll.py

--- jroll/jroll.py
+++ jroll/jroll.py
@@ -69,6 +69,7 @@ class jroll:
             await self.bot.edit_message(message,new_content=str("Rolling\n⠀2\n⠀3\n>4\n⠀5\n⠀1\nRolling"))
             time.sleep(0.9)
             await self.bot.edit_message(message,new_content=str("Rolling\n⠀3\n⠀4\n>5\n⠀1\n⠀2\nRolling"))
+        time.sleep(2.5)
         await self.bot.add_reaction(message,"✅")
 
 def setup(bot):
