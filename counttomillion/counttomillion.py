75235228f0017dce8385f13d5122f0c35411c54e
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2019-05-31 02:11:08 +0100

    Deleted some dumb stuff and changed counttomillion & sugarcog

--- counttomillion/counttomillion.py
+++ counttomillion/counttomillion.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 import json
-
 json="data/counttomillion/count.json"
 fileW=open(json, 'w')
 fileR=open(json, 'r')
-
-data=json.load(fileR)
+try:
+    open(json,"x")
+    fileW.write("{}")
+except:
+    pass
 
 class counttomillion:
     """counttomillion!"""
@@ -17,36 +19,47 @@ class counttomillion:
     @commands.command(pass_context=True)
     async def count(self,ctx):
         """Start the count!"""
+        data=json.load(fileR)
         if ctx.message.author.server_permissions.manage_server:
             if data[ctx.message.channel.id]==0:
                 await self.bot.say("Ok, count to 1 million started! I'll begin!")
-                await self.bot.say("1")
-                data[ctx.message.channel.id]=2
-                await self.bot.edit_channel(ctx.message.channel,topic="Next number: 2")
-                json.dump(data, fp, sort_keys=True, indent=4)
+                data[ctx.message.channel.id]=1
+                await self.bot.edit_channel(ctx.message.channel,topic="Next number: 1")
+                json.dump(data, fileW, sort_keys=True, indent=4)                
             else:
                 await self.bot.say("A count is already running in this channel!")
 
     @commands.command(pass_context=True)
     async def stopcount(self,ctx):
+        data=json.load(fileR)
         if ctx.message.author.server_permissions.manage_server:
             if data[ctx.message.channel.id]==0:
                 await self.bot.say("This channel isn't even counting!")
             else:
                 await self.bot.say("Ok, stopped! We got to {} before stopping!".format(data[ctx.message.channel.id]))
                 data[ctx.message.channel.id]=0
+                json.dump(data, fileW, sort_keys=True, indent=4)
                 await self.bot.edit_channel(ctx.message.channel,topic="")
 
-    @commands.event
     async def on_message(message):
-        if [message.channel.id]!=0
+        data=json.load(fileR)
+        if data[message.channel.id] != 0
             try:
                 content=int(message.content)
-                if content==data[message.channel.id]:
-                    data[message.channel.id]+1
-                    await self.bot.edit_channel(ctx.message.channel,topic="Next number: {}".format(data[message.channel.id]))
-                    json.dump(data, fileW, sort_keys=True, indent=4
             except:
+                try:
+                    await self.bot.delete_message(message)
+                    return
+                except:
+                    return
+            if content==data[message.channel.id]:
+                data[message.channel.id]+=1
+                if data[message.channel.id]==1000000:
+                    await self.bot.say("We have reached a million! Resetting, say 1 to start.")
+                    data[message.channel.id]=1
+                await self.bot.edit_channel(ctx.message.channel,topic="Next number: {}".format(data[message.channel.id]))
+                json.dump(data, fileW, sort_keys=True, indent=4)
+            else:
                 try:
                     await self.bot.delete_message(message)
                 except:
@@ -54,8 +67,3 @@ class counttomillion:
 
 def setup(bot):
     bot.add_cog(counttomillion(bot))
-    if fileR.read()!='':
-        print("Creating count.json file in data/counttomillion/")
-        open(json,'w+').write("{}")
-    else:
-        print("file already exists continuing.")
--- sugarcog/sugarcog.py
+++ sugarcog/sugarcog.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import time
-import random
+import random,asyncio
 
 class sugarcog:
     """Sugarcane farm!!"""
@@ -10,20 +10,20 @@ class sugarcog:
         self.bot = bot
 
     @commands.command()
-    async def sugarcane(self,times=5):
+    async def sugarcane(self,times=2):
         """Sugar Cane!"""
         times=int(times)
         msg=await self.bot.say("<:space:559559231817121793><:observer:559556253807017985><:redstone:559556253081272320>\n<:space:559559231817121793><:piston:559556586650206223><:stone:559556253169483796>\n<:sugarcane:559556252972220478>")
         await self.bot.edit_message(msg,new_content="<:space:559559231817121793><:observer:559556253807017985><:redstone:559556253081272320>\n<:space:559559231817121793><:piston:559556586650206223><:stone:559556253169483796>\n<:sugarcane:559556252972220478>")
         for i in range(times):
-            pause=random.randint(4,15)
-            time.sleep(pause)
+            pause=random.randint(1,8)
+            await asyncio.sleep(pause)
             await self.bot.edit_message(msg,new_content="<:space:559559231817121793><:observer:559556253807017985><:redstone:559556253081272320>\n<:sugarcane:559556252972220478><:piston:559556586650206223><:stone:559556253169483796>\n<:sugarcane:559556252972220478>")
-            pause=random.randint(4,15)
-            time.sleep(pause)
+            pause=random.randint(1,8)
+            await asyncio.sleep(pause)
             await self.bot.edit_message(msg,new_content="<:sugarcane:559556252972220478><:observer:559556253807017985><:redstone:559556253081272320>\n<:sugarcane:559556252972220478><:piston:559556586650206223><:stone:559556253169483796>\n<:sugarcane:559556252972220478>")
-            pause=random.randint(4,15)
-            time.sleep(pause)
+            pause=random.randint(1,8)
+            await asyncio.sleep(pause)
             await self.bot.edit_message(msg,new_content="<:space:559559231817121793><:observer:559556253807017985><:redstone:559556253081272320>\n<:space:559559231817121793><:piston:559556586650206223><:stone:559556253169483796>\n<:sugarcane:559556252972220478>")
         await self.bot.add_reaction(msg,"✅")
 
